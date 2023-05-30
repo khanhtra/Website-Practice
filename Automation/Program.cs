7f419c7ca4d57c6c14924c7e9821d0e7966a3ceb
Your Name <your@email.com>
2023-05-30 00:41:18 -0500

    Added Youtube Video test case

--- Automation/Program.cs
+++ Automation/Program.cs
@@ -20,6 +20,18 @@ class Program
     element.SendKeys("youtube");
     element.SendKeys(Keys.Enter);
     driver.FindElement(By.CssSelector(".DKV0Md")).Click();
+    String URL = "https://www.youtube.com/";
+    Assert.AreEqual(URL, driver.Url);
+    Thread.Sleep(5000);
+    IWebElement searchElement = driver.FindElement(By.CssSelector("input#search"));
+    searchElement.SendKeys("Fly me to the moon");
+    searchElement.SendKeys(Keys.Enter);
+    Thread.Sleep(3000);
+    driver.FindElement(By.LinkText("Fly Me to the Moon/Lucky (Sinatra/Jason Mraz & Colbie Caillat MASHUP) Rick Hale & Breea Guttery")).Click();
+    String VideoURL = "https://www.youtube.com/watch?v=9Hf1t_BXGfk";
+    Assert.AreEqual("https://www.youtube.com/watch?v=9Hf1t_BXGfk", driver.Url);
+
+    
     }
 }
 }
