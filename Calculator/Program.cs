d02abf78d090d592d10d3ae8dd1ba51ed8e42b95
Your Name <your@email.com>
2023-05-31 19:15:06 -0500

    Fixed conversion bug and error handling

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,60 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace Calculator{
-class Program{
-    public static void Main(string[] args){
-Console.WriteLine("Hello, I am a simple calculator using only two numbers");
-Console.WriteLine("Please select two numbers");
-Console.WriteLine("First number?");
-String operation;
-char first;
-char second;
-first = Console.ReadKey().KeyChar;
-
-if (!Char.IsNumber(first)){
-    Console.WriteLine("Please re-run and enter a valid numbers!");
-}
-Console.WriteLine();
-Console.WriteLine("Second number?");
-second = Console.ReadKey().KeyChar;
-
-if (!Char.IsNumber(second)){
-    Console.WriteLine("Please re-run and enter a valid numbers!");
-}
-Console.WriteLine();
-Console.WriteLine("What operation would you like to use [addition, subtraction, multiplication, division]");
-operation = Console.ReadLine();
-
-double add;
-double sub;
-double mul;
-double div;
-
-add = first + second;
-sub = first - second;
-mul = first * second;
-div = first / second;
-
-
-switch(operation)
+namespace Calculator
 {
-    case "addition":
-    Console.WriteLine("The sum is: " + add);
-    break;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I am a simple calculator using only two numbers");
+            Console.WriteLine("Please select two numbers");
+            Console.WriteLine("First number?");
+            String operation;
 
-    case "subtraction":
-    Console.WriteLine("The difference is: " + sub);
-    break;
+            double first = 0;
+            double second = 0;
 
-    case "multiplication":
-    Console.WriteLine("The product is: " + mul);
-    break;
+            try {
+                first = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e){
+                Console.WriteLine("Please enter a valid number!");
+                return;
+            }
 
-    case "division":
-    Console.WriteLine("The quotient is: " + div);
-    break;
+            Console.WriteLine("Second number?");
+
+             try {
+                second = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception e){
+                Console.WriteLine("Please enter a valid number!");
+                return;
+            }
+
+            Console.WriteLine("What operation would you like to use [addition, subtraction, multiplication, division]");
+            operation = Console.ReadLine();
+
+            double add;
+            double sub;
+            double mul;
+            double div;
+           
+
+            add = first + second;
+            sub = Convert.ToDouble(first) - Convert.ToDouble(second);
+            mul = Convert.ToDouble(first) * Convert.ToDouble(second);
+            div = Convert.ToDouble(first) / Convert.ToDouble(second);
 
 
-}
-}
-}
+            switch (operation)
+            {
+                case "addition":
+                    Console.WriteLine("The sum is: " + add);
+                    break;
+
+                case "subtraction":
+                    Console.WriteLine("The difference is: " + sub);
+                    break;
+
+                case "multiplication":
+                    Console.WriteLine("The product is: " + mul);
+                    break;
+
+                case "division":
+                    Console.WriteLine("The quotient is: " + div);
+                    break;
+                default:
+                Console.WriteLine("Please enter a valid operation from the given choices!");
+                break;
+            }
+        }
+    }
 }
